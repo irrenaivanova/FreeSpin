4087fd468d7aad4dc495d1e7c934f770050e9c8e
irrenaivanova <150434502+irrenaivanova@users.noreply.github.com>
2025-10-08 13:43:14 +0300

    Create generic Result<T> and ResultExtensions for  API responses

---
+++ source/FreeSpin.Application/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿namespace FreeSpin.Application.Common;
+
+public class Result<T>
+{
+	private Result(bool isSuccess, T? value, string? error, ErrorType errorType)
+	{
+		IsSuccess = isSuccess;
+		Value = value;
+		Error = error;
+		ErrorType = errorType;
+	}
+	public bool IsSuccess { get; }
+	public string? Error { get; }
+	public ErrorType ErrorType { get; }
+	public T? Value { get; }
+
+	public static Result<T> Success(T value)
+		=> new Result<T>(true, value, null, ErrorType.None);
+
+	public static Result<T> Failure(string error, ErrorType errorType)
+		=> new Result<T>(false,default, error, errorType);
+}
+
+public enum ErrorType
+{
+	None,
+	NotFound,
+	Forbidden,
+	Validation
+}
---
+++ source/FreeSpin.WebAPI/Extensions/ResultExtension.cs
@@ -0,0 +1,26 @@
+﻿using FreeSpin.Application.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeSpin.WebAPI.Extensions;
+
+public static class ResultExtension
+{
+	public static IActionResult ToActionResult<T>(this Result<T> result)
+	{
+		if (result.IsSuccess)
+		{
+			return new OkObjectResult(result.Value);
+		}
+
+		return result.ErrorType switch
+		{
+			ErrorType.NotFound => new NotFoundObjectResult(new { error = result.Error }),
+			ErrorType.Forbidden => new ForbidResult(),
+			ErrorType.Validation => new BadRequestObjectResult(new { error = result.Error }),
+			_ => new ObjectResult(new { error = result.Error })
+			{
+				StatusCode = StatusCodes.Status500InternalServerError,
+			}
+		};
+	}
+}
